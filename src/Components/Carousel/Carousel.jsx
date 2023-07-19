56a92c42f25544cfccd35541b7e7df3beaff2ab3
Jaachi Okafor <okaforjaachi@gmail.com>
2023-07-19 10:31:33 +0100

    added carousel for shows

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from "react";
+
+// import "./styles.css";
+import classes from "./index.module.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import PropTypes from "prop-types";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+const propTypes = {
+  images: PropTypes.array,
+};
+
+const defaultProps = {
+  images: [],
+};
+
+const Carousel = ({ images }) => {
+  return (
+    <div class="w-full">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {images.map((imageUrl, index) => (
+          <SwiperSlide key={index}>
+            <img src={imageUrl} alt={`Image ${index}`} />
+          </SwiperSlide>
+        ))}
+
+        {/* <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide> */}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,95 @@
 import React from "react";
 import Slider from "../../Components/Slider/Slider";
+import Carousel from "../../Components/Carousel/Carousel";
 
 const Homepage = () => {
   return (
     <div>
-      <Slider />
+      <div className="hero_section">
+        <Slider />
+      </div>
+
+      <div class="flex justify-center" className="shows">
+        <div class="w-1/2 px-4">
+          <div>
+            <h2>
+              Kupids Corner <span>See more</span>
+            </h2>
+            <Carousel
+              images={[
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+              ]}
+            />
+          </div>
+
+          <div>
+            <h2>
+              Kupids Corner <span>See more</span>
+            </h2>
+            <Carousel
+              images={[
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+              ]}
+            />
+          </div>
+
+          <div>
+            <h2>
+              Kupids Corner <span>See more</span>
+            </h2>
+            <Carousel
+              images={[
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+                "https://swiperjs.com/demos/images/nature-1.jpg",
+              ]}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
